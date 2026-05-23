5064bc5174be448601332b53761f3e417cdcbd2e
Kenneth Hill <khill@gmail.com>
2026-05-23 19:15:47 -0400

    shout out nix. one of my favorite genders

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+    description = "Dev shell for Telex Mod";
+    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    outputs = { self, nixpkgs }:
+      let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in {
+        devShells.x86_64-linux.default = pkgs.mkShell {
+          CSII_MANAGEDPATH="$HOME/.local/share/Steam/steamapps/common/Cities Skylines II/Cities2_Data/Managed";
+          CSII_MOD_DIRECTORY="$HOME/.local/share/Steam/steamapps/compatdata/949230/pfx/drive_c/users/steamuser/AppData/LocalLow/Colossal Order/Cities Skylines II/.cache/Mods";
+          buildInputs = [
+            pkgs.dotnet-sdk_8
+            pkgs.mono
+            pkgs.just
+          ];
+        };
+      };
+}
